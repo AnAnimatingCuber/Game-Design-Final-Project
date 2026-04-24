4a02cee4ffc1f5f845a779660a6323db347d5bf9
Sophie <sophie.r.bates@gmail.com>
2026-04-24 11:27:44 -0400

    Button is fixed!! Don't know how or why but it's fixed!

--- Assets/Scripts/Transitions.cs
+++ Assets/Scripts/Transitions.cs
@@ -27,7 +27,7 @@ public class Transitions : MonoBehaviour
 
     IEnumerator LoadScene(){
           transitionAnim.SetTrigger("endTransition");
-          yield return new WaitForSeconds(2f);
+          yield return new WaitForSeconds(.5f);
           SceneManager.LoadScene(sceneName);
     }
 
